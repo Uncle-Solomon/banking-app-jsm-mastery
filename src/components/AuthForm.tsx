330494fe1e5717905af07711e7b8504c1fa84e46
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2024-05-21 12:26:44 +0100

    implement auth, appwrite and sentry

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -51,6 +51,7 @@ const AuthForm = ({ type }: { type: string }) => {
           email: data.email,
           password: data.password,
         });
+        // console.log(response);
         if (response) {
           router.push("/");
         }
--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -1,15 +1,66 @@
 "use server";
 
-export const signIn = async () => {
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../server/appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
+
+export const signIn = async ({ email, password }: signInProps) => {
   try {
+    const { account } = await createAdminClient();
+
+    const response = await account.createEmailPasswordSession(email, password);
+
+    return parseStringify(response);
   } catch (error) {
     console.error("Error", error);
   }
 };
 
 export const signUp = async (userData: SignUpParams) => {
+  const { email, password, firstName, lastName } = userData;
   try {
+    const { account } = await createAdminClient();
+
+    const name = `${firstName} ${lastName}`;
+    const newUser = await account.create(ID.unique(), email, password, name);
+
+    const session = await account.createEmailPasswordSession(email, password);
+
+    cookies().set("jsm-bank-app-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    return parseStringify(newUser);
   } catch (error) {
     console.error("Error", error);
+  }
+};
+
+// ... your initilization functions
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    const newUser = await account.get();
+
+    return parseStringify(newUser);
+  } catch (error) {
+    return null;
+  }
+}
+
+export const logoutAccount = async () => {
+  try {
+    const { account } = await createSessionClient();
+
+    cookies().delete("jsm-bank-app-session");
+
+    await account.deleteSession("current");
+  } catch (error) {
+    return null;
   }
 };
